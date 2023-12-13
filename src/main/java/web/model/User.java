676f671db64e7aa583d10336d2bc992941f7c8b4
Alexey030383 <alexey030383@gmail.com>
2023-12-13 17:08:57 +0300

    PP_2.2.3_update

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,12 +1,11 @@
 package web.model;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
 @Table(name = "users")
-public class User implements Serializable {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
